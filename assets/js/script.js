18b287d24f0aa8c878d6a55efd894ddbef497553
cory-eheart-1 <cory.eheart@gmail.com>
2022-07-25 19:50:35 -0500

    edit

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+function openFacebook(){
+    window.open("https://www.facebook.com/BurningGenesis", "_blank");
+  }
+  
+  function openInsta(){
+    window.open("https://www.instagram.com/burninggenesis/", "_blank");
+  }
+  
+  function openTwitter(){
+    window.open("https://twitter.com/burning_genesis", "_blank");
+  }
+  
