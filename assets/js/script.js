e7ed1d8c6cb7cdf10ab99b727e0cf63a2c09c250
cory-eheart-1 <cory.eheart@gmail.com>
2022-07-30 17:55:14 -0500

    edit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,18 @@
 function openFacebook(){
-    window.open("https://www.facebook.com/BurningGenesis", "_blank");
-  }
-  
-  function openInsta(){
-    window.open("https://www.instagram.com/burninggenesis/", "_blank");
-  }
-  
-  function openTwitter(){
-    window.open("https://twitter.com/burning_genesis", "_blank");
-  }
-  
+  window.open("https://www.facebook.com/BurningGenesis", "_blank");
+}
+
+function openInsta(){
+  window.open("https://www.instagram.com/burninggenesis/", "_blank");
+}
+
+function openTwitter(){
+  window.open("https://twitter.com/burning_genesis", "_blank");
+}
+
+function openSpotify(){   window.open("https://open.spotify.com/artist/51b0iJaeN11hl1LU8oUFML?si=k6Qm6n6KTuyWUZ7_jHvmNg");
+}
+
+function openApple(){
+  window.open("https://music.apple.com/us/artist/burning-genesis/1609642313");
+}
