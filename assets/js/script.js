7643210d51e295425315ae7a5eaa16e80446d8ce
cory-eheart-1 <cory.eheart@gmail.com>
2022-08-30 14:07:55 -0500

    mapped all buttons to one link function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,30 @@
-function openFacebook(){
-  window.open("https://www.facebook.com/BurningGenesis", "_blank");
-}
-
-function openInsta(){
-  window.open("https://www.instagram.com/burninggenesis/", "_blank");
-}
-
-function openTwitter(){
-  window.open("https://twitter.com/burning_genesis", "_blank");
-}
-
-function openSpotify(){   window.open("https://open.spotify.com/album/4WY1MYFLytpva8LcUFHY9A");
-}
-
-function openApple(){
-  window.open("https://music.apple.com/us/album/my-o-p-i-c/1641395826");
-}
-
-function openYoutube(){
-  window.open("https://www.youtube.com/watch?v=TvcaGBn9DDU&list=OLAK5uy_lRZEAA3gc7vOUQB6mhSs5dCs9Oi0rt4sQ");
+function openLink(id) {
+  if (id == 'Facebook') {
+    window.open("https://www.facebook.com/BurningGenesis");
+  } 
+  else if (id == 'Instagram') {
+    window.open("https://www.instagram.com/burninggenesis/");
+  } 
+  else if (id == 'Twitter') {
+    window.open("https://twitter.com/burning_genesis");
+  } 
+  else if (id == 'Spotify') {
+    window.open("https://open.spotify.com/artist/51b0iJaeN11hl1LU8oUFML");
+  } 
+  else if (id == 'YouTube') {
+    window.open("https://www.youtube.com/channel/UCZvsrdqhPphNYo3nysHjtBA");
+  } 
+  else if(id == 'YTMusic'){
+    window.open("https://music.youtube.com/channel/UCZvsrdqhPphNYo3nysHjtBA");
+  } 
+  else if (id == 'Apple') {
+    window.open("https://music.apple.com/us/artist/burning-genesis/1609642313");
+  } 
+  else if (id == 'iTunes') {
+    window.open("https://music.apple.com/us/artist/burning-genesis/1609642313");
+  } 
+  else if (id == 'Amazon') {
+    window.open("https://music.amazon.com/artists/B09SF8LGZ6/burning-genesis");
+  }
+  
 }
